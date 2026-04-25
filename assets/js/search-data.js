f654810a0da28a3e869e065a982c5bc3fffeb883
parkminhyung <pmh621@naver.com>
2026-04-25 09:00:59 +0000

    Deploying to gh-pages from @ parkminhyung/parkminhyung.github.io@0e1253d975e1373db8758059b4b2ed9379bbf08c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-performance",
+          title: "Performance",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/performance/";
+          },
+        },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
